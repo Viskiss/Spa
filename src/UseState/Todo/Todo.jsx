d98c2e35799c40f8647919332811bf1ff0107f9a
Ivlay <alekseyguvatagin@gmail.com>
2022-09-09 23:49:18 +0300

    format docements

--- src/UseState/Todo/Todo.jsx
+++ src/UseState/Todo/Todo.jsx
@@ -1,96 +1,91 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
-import { Todos } from "../../constans/data";
-import styles from "./Todo.module.css";
-import  TodoItem  from "./TodoItem";
+import { useEffect, useState } from 'react';
+
+import TodoItem from './TodoItem';
+
+import styles from './Todo.module.css';
 
 function Todo(props) {
+	const [value, setValue] = useState('');
+	const [result, setResult] = useState('');
+	const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos') || '[]'));
 
-  const [value, setValue] = useState("");
-  // const [todos, setTodos] = useState(Todos);
-  const [result, setResult] = useState("");
-  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todos") || "[]")
-  );
-
-  useEffect(
+	useEffect(
     () => {
-      localStorage.setItem("todos", JSON.stringify(todos));
-    },
+		  localStorage.setItem('todos', JSON.stringify(todos));
+	  },
     [todos],
   );
 
-  const setTodosWithSave = (todos) => {
-    setTodos(todos);
-  };
+	const setTodosWithSave = (todos) => {
+		setTodos(todos);
+	};
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    if (value.trim()) {
-      setTodos([...todos, { id: Date.now(), value: value }]);
-    }
-    console.log(value);
-    setValue("");
-  };
+	const submitHandler = (e) => {
+		e.preventDefault();
 
-  const handleDeleteTodo = (id) => {
-    const newTodos =todos.filter((todo) => todo.id !== id);
-    setTodosWithSave(newTodos);
-  };
+		if (value.trim()) {
+			setTodos([...todos, { id: Date.now(), value: value }]);
+		}
 
-  const handleChangeInput = (event) => {
-    setValue(event.target.value);
-  };
+		setValue('');
+	};
 
-  // const changeInputTodo = (event, value) => {
-  //   setResult(event.target.value);
-  // };
+	const handleDeleteTodo = (id) => {
+		const newTodos = todos.filter((todo) => todo.id !== id);
 
-  const submitInputTodo = () => {
-    if (result.length !== 0) {
-      redactedTodo();
-    } else {
-      alert("empty todo!");
-    }
-    setResult("");
-  };
+		setTodosWithSave(newTodos);
+	};
 
-  const redactedTodo = (value, id) => {
-     const findTodo = todos.find((todo) => todo.id === id)
-    if(!value) {
-      handleDeleteTodo(id)
-    } else if (findTodo) {
- findTodo.value = value
-      setTodos([...todos]);
-    }
-  };
+	const handleChangeInput = (event) => {
+		setValue(event.target.value);
+	};
 
-  return (
-    <div className={styles.input}>
-      <form className={styles.form} onSubmit={submitHandler}>
-        <input value={value} onChange={handleChangeInput} type="value" />
-        <button type="submit">Add</button>
-      </form>
-      {/* <todoItem /> */}
-      <div className={styles.result}>
-        {todos.map((todo) => (
-          <TodoItem redactedTodo={redactedTodo} submitInputTodo={submitInputTodo}  handleDeleteTodo={handleDeleteTodo} todo={todo} key={todo.id}/>
-        ))}
-        {/* {todos.map(({ value, id }) => (
-          <div className={styles.itemTodo} key={id}>
-            <form onSubmit={submitInputTodo}>
-              {value}
-              <input
-                id={id}
-                onChange={changeInputTodo}
-                value={result}
-                type="text"
-              />
-            </form>
-            <button onClick={handleDeleteTodo.bind(null, id)}>Delete</button>
-          </div>
-        ))} */}
-      </div>
-    </div>
-  );
+	const submitInputTodo = () => {
+		if (result.length !== 0) {
+			redactedTodo();
+		} else {
+			alert('empty todo!');
+		}
+
+		setResult('');
+	};
+
+	const redactedTodo = (value, id) => {
+		const findTodo = todos.find((todo) => todo.id === id);
+
+		if (findTodo) {
+			if (!value) {
+				handleDeleteTodo(id);
+			} else {
+				findTodo.value = value;
+
+				setTodos([...todos]);
+			}
+		}
+	};
+
+	return (
+		<div className={styles.input}>
+			<form className={styles.form} onSubmit={submitHandler}>
+				<input
+          type='value'
+          value={value}
+          onChange={handleChangeInput}
+        />
+				<button type='submit'>Add</button>
+			</form>
+			<div className={styles.result}>
+				{todos.map((todo) => (
+					<TodoItem
+            todo={todo}
+						key={todo.id}
+						redactedTodo={redactedTodo}
+						submitInputTodo={submitInputTodo}
+						handleDeleteTodo={handleDeleteTodo}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 export default Todo;
--- src/UseState/Todo/TodoItem/index.jsx
+++ src/UseState/Todo/TodoItem/index.jsx
@@ -1,43 +1,42 @@
-import styles from '../Todo.module.css'
-import React, { useState } from "react";
+import { useState } from 'react';
 
- function TodoItem(props){
-  const {
-    todo: {
-      id,
-      value,
-    },
-    handleDeleteTodo,
-    redactedTodo
-  } = props;
-  
-  const [todoValue, setTodoValue] = useState(value);
+import styles from '../Todo.module.css';
 
+function TodoItem(props) {
+	const {
+		todo: { id, value },
+		handleDeleteTodo,
+		redactedTodo,
+	} = props;
 
-  const changeInputTodo = (event, value) => {
-    setTodoValue(event.target.value);
-  };
+	const [todoValue, setTodoValue] = useState(value);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    
-    console.log(todoValue)
-    redactedTodo(todoValue, id)
-  }
-  
+	const changeInputTodo = (event) => {
+		setTodoValue(event.target.value);
+	};
 
-    return(
-        <div className={styles.result}>
-  
-          <div className={styles.itemTodo} key={id}>
-            <form onSubmit={onSubmit} >
-              <input id={id} onBlur={onSubmit} onChange={changeInputTodo} value={todoValue} type="text" />
-            </form>
-            <button onClick={handleDeleteTodo.bind(null,id)}>Delete</button>
-          </div>
-    
-      </div>
-    )
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		redactedTodo(todoValue, id);
+	};
+
+	return (
+		<div className={styles.result}>
+			<div className={styles.itemTodo} key={id}>
+				<form onSubmit={onSubmit}>
+					<input
+						type='text'
+						id={id}
+						value={todoValue}
+						onBlur={onSubmit}
+						onChange={changeInputTodo}
+					/>
+				</form>
+				<button onClick={handleDeleteTodo.bind(null, id)}>Delete</button>
+			</div>
+		</div>
+	);
 }
 
-export default TodoItem
+export default TodoItem;
